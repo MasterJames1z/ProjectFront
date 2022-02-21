e757d4d1d1cea505b1984709af41ca56e80cca14
Techatham Panthumsinchai <Bay_Techatham@Techatham.local>
2022-02-21 17:25:25 +0700

    add simple sort

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const ejs = require("ejs");
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+var sortStatus = true;
+
 app.get("/", function(req,res){
-    res.render("home");
+    res.render("home",{sortStatus: sortStatus});
 })
 
 
 app.listen(3000,function(){
-    console.log("sever staart port 3000");
+    console.log("sever start port 3000");
 })
