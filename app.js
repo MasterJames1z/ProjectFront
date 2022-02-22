2e543e6e06afceea277a21aeff9c5f1969cd4a40
Techatham Panthumsinchai <Bay_Techatham@Techatham.local>
2022-02-22 19:46:45 +0700

    add change status button

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const ejs = require("ejs");
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-var sortStatus = true;
+var sort = true;
+var orderList = [{menuName: "ผัดกระเพา", remark: ["หอมมะลิ"], status: "cooking"}, {menuName: "ผัดผัก", remark: ["AAA", "BBB"], status: "queue"}];
 
 app.get("/", function(req,res){
-    res.render("home",{sortStatus: sortStatus});
+    res.render("home",{sort: sort, orderList: orderList});
 })
 
+app.get("/cus", function(req,res){
+
+})
 
 app.listen(3000,function(){
     console.log("sever start port 3000");
