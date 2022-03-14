c914d9f2bdd4abc01b9e5f86116147d184ce3ef4
Worawut Aunkham <jameskater350@gmail.com>
2022-03-14 19:27:17 +0700

    Move server.js to public folder

---
+++ public/server.js
@@ -0,0 +1,22 @@
+// document.getElementById("chtable").innerHTML("table");
+// console.log("true");
+
+// let array = ["a", "b", "c"];
+// let index = 1;
+// array.splice(index, 1);
+
+
+// console.log(array);
+
+// const express = require("express");
+// const app = express()
+
+// const users =[]
+
+// app.get('/users',(req,res)=>{
+//     res.json(users)
+// })
+
+// app.listen(3050)
+
+
